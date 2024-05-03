5bdad891d5a84ff9a056274669499f75d5288e87
Kwan Wai Teng <waiteng@deepdivestudios.gg>
2024-05-03 20:00:51 +0800

    First pass input handling
    Using the new input system from Unity,
    
    1. Added input actions asset for the game
    2. Handle these input in PlayerController
    3. New scripts. player controller, character, consts. Self explanatory
    4. Added placeholder prefabs for player controller & character
    5. Basic PlayerInputManagerProxy handling in the prefab. Might change the name, but it handles join/leave logic essentially. Could reuse for networking?
    
    Also reimported TMP assets because unity is being funny when i click tmp settings??

---
+++ Assets/Scripts/Player/PlayerConsts.cs
@@ -0,0 +1,8 @@
+public static class PlayerConsts
+{
+    #region Input
+    public const string PlayerInputMap_Start = "Start";
+    public const string PlayerInputMap_Gameplay = "Gameplay";
+    public const string PlayerInputMap_UI = "UI";
+    #endregion
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+
+/// <summary>
+/// Input controller/reader etc.
+/// Reference: 
+/// https://docs.unity3d.com/Packages/com.unity.inputsystem@1.5/manual/PlayerInput.html
+/// https://docs.unity3d.com/Packages/com.unity.inputsystem@1.5/manual/Interactions.html
+/// </summary>
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Identification")]
+    [field: SerializeField, ReadOnly]
+    public int PlayerIndex { get; private set; } = -1;
+    private string m_uId = string.Empty;
+    public string UniqueId => m_uId;
+
+    [Header("Character")]
+    [SerializeField, ReadOnly]
+    private PlayerCharacter m_character;
+
+    //event system/input
+    [Header("Input")]
+    [SerializeField, ReadOnly]
+    private PlayerInput m_pInputControls;
+    private MultiplayerEventSystem m_eventSystem;
+
+    private void OnValidate()
+    {
+        m_pInputControls = GetComponentInChildren<PlayerInput>();
+    }
+
+    private void Awake()
+    {
+        m_pInputControls = GetComponentInChildren<PlayerInput>();
+        ToggleInputActive(true);
+    }
+
+    public void AssignId(int id)
+    {
+        Debug.Log($"Player id changed from {PlayerIndex} to {id}");
+        PlayerIndex = id;
+    }
+
+    public void AssignUniqueId(string id)
+    {
+        m_uId = id;
+    }
+
+    #region Input
+    #region Input Map Stuff
+    private void SwitchInputMap(string map)
+    {
+        m_pInputControls.SwitchCurrentActionMap(map);
+    }
+
+    /// <summary>
+    /// Use at your own risk
+    /// </summary>
+    /// <param name="active"></param>
+    private void ToggleInputActive(bool active = true)
+    {
+        if (active)
+        {
+            m_pInputControls.ActivateInput();
+        }
+        else
+        {
+            m_pInputControls.DeactivateInput();
+        }
+    }
+    #endregion
+
+    #region Event Listeners - Start
+    public void OnStart()
+    {
+        print("Joined!");
+        SwitchInputMap(PlayerConsts.PlayerInputMap_Gameplay);
+    }
+    #endregion
+
+    #region Event Listeners - Gameplay
+    public void OnJump()
+    {
+        print("Jumped");
+
+        m_character.OnJumpInput();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveAxisVal = value.Get<Vector2>();
+        
+        m_character.OnMoveInput(moveAxisVal);
+    }
+
+    public void OnAttack()
+    {
+        print("attack!!!");
+        m_character.OnAttackInput();
+    }
+    #endregion
+
+    #region Event System (UI)
+    #endregion
+    #endregion
+
+    public void SetCharacter(PlayerCharacter character)
+    {
+        m_character = character;
+        character.SetOwner(this);
+    }
+}
