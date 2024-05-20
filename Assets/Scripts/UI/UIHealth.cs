3f10443f38a646dea148a8e2049d8bbb05d8156a
Kwan Wai Teng <waiteng@deepdivestudios.gg>
2024-05-20 19:51:22 +0800

    adjustment to the UI png settingts
    adjustment to UI

--- Assets/Scripts/UI/UIHealth.cs
+++ Assets/Scripts/UI/UIHealth.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -11,13 +9,19 @@ public class UIHealth : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI m_hpTxt;
 
+    [SerializeField, Range(0f, 1f)]
     private float m_percent;
+
+    private void OnValidate()
+    {
+         SetHealth(m_percent);
+    }
 
     public void SetHealth(float percent)
     {
         m_percent = percent;
         m_baseSlider.value = m_percent;
-        m_hpTxt.text = Mathf.CeilToInt(m_percent).ToString() + "%";
+        m_hpTxt.text = Mathf.CeilToInt(m_percent * 100f).ToString() + "%";
     }
 
     public void SetHealth(float curr, float max)
