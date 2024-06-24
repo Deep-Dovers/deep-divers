ce7aa0fcafe937e2e3141e6a7baaea6c95bad32e
Benson Ma <bensonma889@gmail.com>
2024-06-24 10:49:09 +0800

    AI wip

--- Assets/Scripts/Ai/BaseEnemyBehaviour.cs
+++ Assets/Scripts/Ai/BaseEnemyBehaviour.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Pathfinding;
 using UnityEngine;
 
 public class BaseEnemyBehaviour : MonoBehaviour
@@ -19,17 +20,42 @@ public class BaseEnemyBehaviour : MonoBehaviour
     //! Temp solution to show enemy hp
     public TextMesh m_textMesh;
 
-    public float DetectionRange;
+    [Space, Header("Detection Variable")]
+    [SerializeField, Tooltip("The Fov Angle")]
+    public float fovAngle = 60f;
+    [SerializeField, Tooltip("Range of Ai Vision")]
+    public float fovRange = 2.6f;
+    [SerializeField, Tooltip("Direction of the Fov")]
+    public Vector2 direction;
+
+    [SerializeField]
+    Transform[] m_patrolPath;
+
+    Path m_path;
+    Seeker m_seeker;
+    Rigidbody2D m_rigidbody;
+    int m_currentWaypoint = 0;
+    AiActionStateState m_curState;
+    AiActionStateState m_prevState;
+
     private bool hasLineOfSight = false;
+    private GameObject m_playerRefs;
     // Start is called before the first frame update
     void Start()
     {
         m_curHP = m_Hp; 
+        m_seeker = GetComponent<Seeker>();
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_seeker.StartPath(m_rigidbody.position, m_patrolPath[0].position);
+    }
+    void OnPathComplete(Path p)
+    {
+        
     }
     // Update is called once per frame
     void Update()
     {
-
+        //if()
     }
     private void FixedUpdate()
     {
@@ -45,12 +71,14 @@ public class BaseEnemyBehaviour : MonoBehaviour
             Kill();
         }
     }
-    public void Kill()
+    public virtual void Kill()
     {
         m_textMesh.color = Color.red;
         m_textMesh.text = "I think i died";
         Destroy(gameObject, 2f);
     }
+    public void UpdateAiState()
+    {
 
-
+    }
 }
